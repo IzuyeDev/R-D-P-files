1f36864e65beb2205f30c4f9730befe290e21c6f
RizeKy <86692565+RizeKy@users.noreply.github.com>
2021-09-18 01:52:21 +0700

    up

---
+++ files/xampp-download.bat
@@ -0,0 +1,8 @@
+@echo off
+echo.
+echo.
+cmd /c wget -P C:\Users\administrator\ https://pilotfiber.dl.sourceforge.net/project/xampp/XAMPP%20Windows/7.4.3/xampp-windows-x64-7.4.3-0-VC15.zip
+::powershell Invoke-WebRequest https://pilotfiber.dl.sourceforge.net/project/xampp/XAMPP%20Windows/7.4.3/xampp-windows-x64-7.4.3-0-VC15.zip -OutFile C:\Users\administrator\Desktop\xampp-install.zip
+echo.
+echo.
+exit
